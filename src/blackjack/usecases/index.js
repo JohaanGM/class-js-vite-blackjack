e1964b88fc1e631b2f526b16c867730e202b8dea
Johan <johanjgmendez@gmail.com>
2025-06-16 14:32:07 -0400

    First commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,7 @@
+
+//Archivo Barril
+export {crearCartaHTML} from './crear-carta-html';
+export {crearDeck} from './crear-deck.js';
+export {pedirCarta} from './pedir-carta.js';
+export {turnoComputadora} from './turno-computadora';
+export {valorCarta} from './valor-carta.js';
